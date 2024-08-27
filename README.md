1c3d48c5d36a1dd1a78660e2bc31dda48fd3089a
Hunter_H <qhx0725@vip.qq.com>
2024-08-27 21:54:19 +0800

    添加models

--- README.md
+++ README.md
@@ -2,3 +2,9 @@
 
 - 本项目使用 BootStrap + Django + Mysql, Echarts🤗
 - doc📖: [飞书](https://rcnnsv5zghgq.feishu.cn/wiki/HL75wi4xui8n20kOFtOcZeSGnOc?from=from_copylink)
+
+## 启动服务器
+```bash
+python manage.py runserver
+```
+> **注意运行前请配置config.json,详情见config目录下readme.md**
--- accounting/models.py
+++ accounting/models.py
@@ -1,3 +1,75 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class NormalUser(models.Model):
+    name = models.CharField(max_length=100)
+
+class Currency(models.Model):
+    name = models.CharField("货币名称", max_length=100)
+    icon = models.CharField("货币图标", max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['id']
+
+
+class Account(models.Model):
+    name = models.CharField("账户类型名", max_length=100)
+    amount = models.DecimalField("账户余额" ,max_digits=8, decimal_places=2)
+    currency = models.ForeignKey(Currency, on_delete=models.SET_NULL, null=True, default=1)
+    icon = models.CharField(max_length=100, null=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['created_date']
+
+
+class Category(models.Model):
+    CATEGORY_TYPES = (
+       ("收入", "income"),
+       ("支出", "expense")
+    )
+    name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=100)
+    category_type = models.CharField(choices=CATEGORY_TYPES, default=CATEGORY_TYPES[0][1], max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['id']
+
+
+class SubCategory(models.Model):
+    name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=100)
+    parent = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['id']
+
+
+class HistoryRecord(models.Model):
+    username = models.ForeignKey(NormalUser, on_delete=models.SET_NULL, null=True)
+    account = models.ForeignKey(Account, on_delete=models.SET_NULL, null=True, default=1)
+    time_of_occurrence = models.DateTimeField(default=timezone.now)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    sub_category = models.ForeignKey(SubCategory, on_delete=models.SET_NULL, null=True, blank=True)
+    currency = models.ForeignKey(Currency, on_delete=models.SET_NULL, null=True, default=1)
+    amount = models.DecimalField(max_digits=8, decimal_places=2)
+    comment = models.CharField(max_length=500, null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    updated_date = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        ordering = ['-time_of_occurrence']
