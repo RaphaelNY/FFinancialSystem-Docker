3df87fcd87aafc66a73168200eb0fd286de611ab
Vegtdog <119641234+vetgdog@users.noreply.github.com>
2024-08-28 09:54:22 +0800

    注册功能后端 (#6)
    Co-authored-by: HunterQu <1762011587@qq.com>

--- accounting/urls.py
+++ accounting/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
 
+from django.urls import path
+from .views import register
+
 urlpatterns = [
     path('', views.index, name='index'), #收支记账主页面
     path('login/', views.login, name='login'),
+    path('register/', register, name='register'),
 ]
--- accounting/views.py
+++ accounting/views.py
@@ -1,5 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login
+
+#注册
+def register(request):
+    if request.method == 'GET':
+        return render(request, 'basic/register.html')
+
+    elif request.method == 'POST':
+        user_name = request.POST.get('username', '')
+        email = request.POST.get('email', '')
+        pwd = request.POST.get('password', '')
+
+        if User.objects.filter(username=user_name).exists():
+            return JsonResponse({'code': 200, 'msg': '用户已存在'})
+
+        user = User.objects.create_user(username=user_name, password=pwd, email=email)
+        user.save()
+
+        return JsonResponse({'code': 200, 'msg': '用户注册成功'})
+
+    return JsonResponse({'code': 403, 'msg': '被禁止的请求'})
+
 
 def index(request):
     return  render(request, 'accounting/index.html')
