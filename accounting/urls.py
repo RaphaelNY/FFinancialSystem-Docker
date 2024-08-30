2317ebfe60348c485ce30a0ef8b2fc89127cd11d
Vegtdog <119641234+vetgdog@users.noreply.github.com>
2024-08-30 09:00:41 +0800

    Dev l (#11)
    * finished login&logout
    
    * 完善登录功能

--- accounting/urls.py
+++ accounting/urls.py
@@ -6,7 +6,8 @@ from .views import register
 
 urlpatterns = [
     path('', views.index, name='index'), #收支记账主页面
-    path('login/', views.login, name='login'),
+    path('login/', views.login_in, name='login_in'),  # 登录
     path('register/', register, name='register'),
+    path('logout/', views.logout_, name='logout_'),
     path('retrieve_category/', views.retrieve_category, name='retrieve_category'),
 ]
--- accounting/views.py
+++ accounting/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.http import JsonResponse
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth import authenticate, logout
+from django.contrib import messages
+
 
 #注册
 def register(request):
@@ -53,3 +56,34 @@ def retrieve_category(request):
     return JsonResponse({"categories": category_list})
 def login(request):
     return  render(request, 'accounting/login.html')
+
+
+
+    #登录
+def login_in(request):
+    if request.method == 'GET':
+        return render(request, 'accounting/login.html')
+
+    elif request.method == 'POST':
+        user_name = request.POST.get('username', '')
+        pwd = request.POST.get('password', '')
+
+        user = authenticate(username=user_name, password=pwd)
+
+        if user:
+            if user.is_active:
+                auth_login(request, user)
+                return redirect('/accounting/')
+            else:
+                return render(request, 'accounting/login.html', {'login_failed': True, 'msg': '用户未激活'})
+        else:
+            # 设置错误消息，并返回登录页面
+            return render(request, 'accounting/login.html', {'login_failed': True, 'msg': '账户名或密码错误，请重新登录'})
+
+    # 处理其他请求方法
+    return JsonResponse({'code': 405, 'msg': '方法不允许'}, status=405)
+
+
+def logout_(request):
+	logout(request)
+	return redirect('/accounting/login')
